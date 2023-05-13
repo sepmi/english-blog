34170ab423110bb96ca12e6aa44e190e1af86046
sepehr mir <sepehr.khabaloo@yahoo.com>
2023-05-13 09:24:38 +0330

    complete videoPosts but with little changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import ShowCase from "./components/ShowCase";
 import Skills from "./components/Skills";
 import SocialsInfo from "./components/SocialsInfo";
 import UserComments from "./components/UserComments";
+import VideoPosts from "./components/VideoPosts";
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <UserComments />
       <SocialsInfo />
       <ShowCase />
+      <VideoPosts />
     </div>
   );
 }
